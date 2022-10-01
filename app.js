a898f2c7a19826913d1c14d7b2d7e98f01955538
IzumiSy <beetle-noise@gmx.com>
2022-10-01 12:12:54 +0900

    Prettified js

--- app.js
+++ app.js
@@ -1,72 +1,75 @@
-const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-require('dotenv').config();
-const stripe = require('stripe');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+require("dotenv").config();
+const stripe = require("stripe");
 
 var app = express();
 
 // view engine setup (Handlebars)
-app.engine('hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }))
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json({}));
 
 /**
  * Home route
  */
-app.get('/', function(req, res) {
-  res.render('index');
+app.get("/", function (req, res) {
+  res.render("index");
 });
 
 /**
  * Checkout route
  */
-app.get('/checkout', function(req, res) {
+app.get("/checkout", function (req, res) {
   // Just hardcoding amounts here to avoid using a database
   const item = req.query.item;
   let title, amount, error;
 
   switch (item) {
-    case '1':
-      title = "The Art of Doing Science and Engineering"
-      amount = 2300      
+    case "1":
+      title = "The Art of Doing Science and Engineering";
+      amount = 2300;
       break;
-    case '2':
-      title = "The Making of Prince of Persia: Journals 1985-1993"
-      amount = 2500
-      break;     
-    case '3':
-      title = "Working in Public: The Making and Maintenance of Open Source"
-      amount = 2800  
-      break;     
+    case "2":
+      title = "The Making of Prince of Persia: Journals 1985-1993";
+      amount = 2500;
+      break;
+    case "3":
+      title = "Working in Public: The Making and Maintenance of Open Source";
+      amount = 2800;
+      break;
     default:
       // Included in layout view, feel free to assign error
-      error = "No item selected"      
+      error = "No item selected";
       break;
   }
 
-  res.render('checkout', {
+  res.render("checkout", {
     title: title,
     amount: amount,
-    error: error
+    error: error,
   });
 });
 
 /**
  * Success route
  */
-app.get('/success', function(req, res) {
-  res.render('success');
+app.get("/success", function (req, res) {
+  res.render("success");
 });
 
 /**
  * Start server
  */
 app.listen(3000, () => {
-  console.log('Getting served on port 3000');
+  console.log("Getting served on port 3000");
 });
