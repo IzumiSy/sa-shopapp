aa115f0deccd93e0178b99e178cc3455e39459b3
IzumiSy <beetle-noise@gmx.com>
2022-10-01 21:39:05 +0900

    Implemented payment confirmation

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -8,3 +8,15 @@ const elements = stripe.elements({
 // Create and mount the Payment Element
 const paymentElement = elements.create("payment");
 paymentElement.mount("#payment-element");
+
+const form = document.querySelector("#payment-form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const { error } = await stripe.confirmPayment({
+    elements,
+    confirmParams: {
+      return_url: "http://localhost:3000/success",
+    },
+  });
+});
