bd3d006b623a9f5616123605bec0c6df3fe21e71
IzumiSy <beetle-noise@gmx.com>
2022-10-01 12:23:55 +0900

    Added call of payment intents

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const stripe = require("stripe")("sk_test_09l3shTSTKHYCzzZZsiLl2vA");
 require("dotenv").config();
-const stripe = require("stripe");
 
 var app = express();
 
@@ -34,11 +34,21 @@ app.get("/checkout", function (req, res) {
   const item = req.query.item;
   const { title, amount, error } = getItemByID(item);
 
-  res.render("checkout", {
-    title: title,
-    amount: amount,
-    error: error,
-  });
+  stripe.paymentIntents
+    .create({
+      amount: amount,
+      currency: "usd",
+      automatic_payment_methods: { enabled: true },
+    })
+    .then((intent) => {
+      console.log(intent);
+
+      res.render("checkout", {
+        title: title,
+        amount: amount,
+        error: error,
+      });
+    });
 });
 
 const getItemByID = (id) => {
